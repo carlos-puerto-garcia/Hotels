fdd39f6cfc08d33f660e1b335cc101f618a74676
Carlos Puerto García <cpuertog@gmail.com>
2020-01-05 20:03:37 -0500

    updating data.js

--- API/src/data.js
+++ API/src/data.js
@@ -2,22 +2,30 @@ const hotels = [
   {
     id: '1',
     name: 'Hotel Dann',
-    stars: 5
+    stars: 5,
+    images: [],
+    price: 200
   },
   {
     id: '2',
     name: 'Hotel Monasterio',
-    stars: 5
+    stars: 5,
+    images: [],
+    price: 150
   },
   {
     id: '3',
     name: 'Hotel Camino Real',
-    stars: 5
+    stars: 4,
+    images: [],
+    price: 100
   },
   {
     id: '4',
     name: 'Hotel Camino Viejo',
-    stars: 5
+    stars: 4,
+    images: [],
+    price: 126
   },
 ];
 
