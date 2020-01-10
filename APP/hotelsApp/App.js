6af5e39ca2f98d66aa6b30d7ee69ad540625dadd
Carlos Puerto García <cpuertog@gmail.com>
2020-01-10 14:39:08 -0500

    updating

--- APP/hotelsApp/App.js
+++ APP/hotelsApp/App.js
@@ -1,16 +1,66 @@
-import React from 'react'
-import { YellowBox } from 'react-native'
-import EventList from './EventList'
-
-YellowBox.ignoreWarnings([
-  'Warning: componentWillMount is deprecated',
-  'Warning: componentWillRecieveProps is deprecated'
-])
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    }
+  }
+
+  componentDidMount () {
+    return fetch('http://localhost:3000/hotels')
+      .then ((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.hotels,
+        })
+      })
+    .catch((error) => {
+        console.log(error)
+    })  
+  }
+
   render () {
-    return (
-      <EventList />
-    )  
+    if (this.state.isLoading){
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      )
+    } else {
+      let hotels = this.state.dataSource.map((val, key) => {
+        return <View key={key} style={styles.item}><Text>{val.name}</Text>
+        </View>
+      })
+      return (
+        <View style={styles.container}>
+          {hotels}
+        </View>
+      )
+    }  
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  item: {
+    flex: 1,
+    alignSelf: 'stretch',
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  }
+})
--- README.md
+++ README.md
@@ -4,7 +4,52 @@
 
 ## API está construido con Node.js y Express
 * Se ejecuta con npm run dev
-
+* En el navegador entrar a la url: http://localhost:3000/hotels
+* Traerá la lista de hoteles:
+```
+[
+  {
+    "id": "1",
+    "name": "Hotel Dann",
+    "stars": 5,
+    "images": [],
+    "price": 200
+  },
+  {
+    "id": "2",
+    "name": "Hotel Monasterio",
+    "stars": 5,
+    "images": [],
+    "price": 150
+  },
+  {
+    "id": "3",
+    "name": "Hotel Camino Real",
+    "stars": 4,
+    "images": [],
+    "price": 100
+  },
+  {
+    "id": "4",
+    "name": "Hotel Camino Viejo",
+    "stars": 4,
+    "images": [],
+    "price": 126
+  }
+]
+```
+* Hotel con id: http://localhost:3000/hotels/3
+```
+[
+  {
+    "id": "3",
+    "name": "Hotel Camino Real",
+    "stars": 4,
+    "images": [],
+    "price": 100
+  }
+]
+```
 
 ## APP está construido con Expo Cli
 * Se ejecuta con yarn dev
